5bca940c463f06786e55b985d1f4bbc92fc5d83a
Daniel <daniel.feldman303@gmail.com>
2026-03-25 21:45:32 +0200

    perf: switch to gpu hardware

--- server/src/config.py
+++ server/src/config.py
@@ -1,13 +1,13 @@
 # STT CONFIG
-STT_MODEL="base"
-STT_DEVICE="cpu"
+STT_MODEL="small"
+STT_DEVICE="cuda"
 STT_COMPUTE_TYPE="int8"
 
 # LLM CONFIG
-LLM_MODEL="gemma3:1b"
+LLM_MODEL="gemma3:4b"
 LLM_SYSTEM_PROMPT="You are Jarvis. Reply in 2 sentences max and english characters only. No exceptions"
 MAX_HISTORY_LENGTH=5
 
 # TTS CONFIG
 PIPER_EXE_PATH=r"C:\piper\piper.exe"
-PIPER_VOICE_MODEL_PATH=r"C:\piper\en_US-amy-medium.onnx"
+PIPER_VOICE_MODEL_PATH=r"C:\piper\en_US-hfc_male-medium.onnx"
