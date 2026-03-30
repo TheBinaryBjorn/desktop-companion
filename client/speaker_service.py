c18e00b3661d1297975b40cb74bd2a236191ee3c
Daniel <daniel.feldman303@gmail.com>
2026-03-30 08:33:50 +0300

    fix: set SPEAKING to LISTENING delay to 1 sec

--- client/speaker_service.py
+++ client/speaker_service.py
@@ -13,7 +13,7 @@ def speaker_loop(brain, playback_queue):
         audio_data = playback_queue.get()
 
         if audio_data == b'EOF':
-            time.sleep(0.5)
+            time.sleep(1)
             brain.set_state(JarvisState.LISTENING)
         else:
             stream.write(audio_data)
