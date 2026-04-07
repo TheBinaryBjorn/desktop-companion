8d4333439d2aa1b33f441ead126df5e3f1065ba0
Daniel <daniel.feldman303@gmail.com>
2026-04-07 17:21:06 +0300

    fix(microphone_service): update config import path

--- client/src/hardware_services/microphone_service.py
+++ client/src/hardware_services/microphone_service.py
@@ -5,7 +5,7 @@ INMP microphone.
 
 from abc import ABC, abstractmethod
 import pyaudio
-from src.hardware_services.config import (
+from src.config import (
     MICROPHONE_SERVICE_FORMAT,
     MICROPHONE_SERVICE_CHANNELS,
     MICROPHONE_SERVICE_RATE,
