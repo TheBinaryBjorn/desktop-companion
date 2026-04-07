09bab4de8b416b815e35889915d59fb6cd75502e
Daniel <daniel.feldman303@gmail.com>
2026-04-07 13:18:18 +0300

    feat(client): add main loop skeleton

---
+++ client/src/client.py
@@ -0,0 +1,17 @@
+"""
+This module contains the main client loop
+that runs on the Raspberry Pi Zero 2W
+"""
+
+def main():
+    """This is the main pi loop"""
+    while True:
+        # listen for input
+        # if wakeword detected
+            # listen for user input
+            # if user spoke
+                # send query to server
+                # wait for response
+        pass
+
+main()
