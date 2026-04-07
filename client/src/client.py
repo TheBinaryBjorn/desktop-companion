21c2e686f919cff67d108e24a67e957b6855b621
Daniel <daniel.feldman303@gmail.com>
2026-04-07 15:42:24 +0300

    feat(architecture): add services skeletons

--- client/src/client.py
+++ client/src/client.py
@@ -3,18 +3,31 @@ This module contains the main client loop
 that runs on the Raspberry Pi Zero 2W
 """
 
+from src.hardware_services import (
+    microphone_service,
+    network_service,
+    screen_service,
+    speaker_service,
+)
+
 
 def main():
     """This is the main pi loop"""
+    microphone = PyAudioMicrophoneService()
+    wakeword = OpenWakeWordService()
+    vad = WebRtcService()
+    server = WebSocketService()
+    speaker = PyAudioSpeakerService()
     while True:
         # listen for input
+
         # if wakeword detected - openwakeword
-            # listen for user input
-            # if user spoke - webrtc vad
-                # listen for full query
-                # stream query to server
-            # wait for response
-            # play server response
+        # listen for user input
+        # if user spoke - webrtc vad
+        # listen for full query
+        # stream query to server
+        # wait for response
+        # play server response
         pass
 
 
