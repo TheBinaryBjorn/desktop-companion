34ec124b65e60d46480f97bc3413ff2446bfc2dd
Daniel <daniel.feldman303@gmail.com>
2026-04-07 17:07:58 +0300

    feat: add wakeword service
    - implement wakeword service using openwakeword

--- client/src/client.py
+++ client/src/client.py
@@ -3,21 +3,30 @@ This module contains the main client loop
 that runs on the Raspberry Pi Zero 2W
 """
 
-from src.hardware_services import (
-    microphone_service,
-    network_service,
-    screen_service,
-    speaker_service,
-)
+"""
+
+from src.hardware_services.speaker_service import PyAudioSpeakerService
+from src.hardware_services.network_service import WebSocketService
+from src.hardware_services.screen_service import ScreenService
+
+from src.software_services.speech_detection_service import WebRtcService
+"""
+from src.hardware_services.microphone_service import PyAudioMicrophoneService
+from src.software_services.wakeword_service import OpenWakeWordService
+from config import WAKEWORD_MODEL_PATH
+from openwakeword.model import Model
 
 
 def main():
     """This is the main pi loop"""
     microphone = PyAudioMicrophoneService()
-    wakeword = OpenWakeWordService()
+    wakeword_model = Model(WAKEWORD_MODEL_PATH)
+    wakeword = OpenWakeWordService(model=wakeword_model)
+    """
     vad = WebRtcService()
     server = WebSocketService()
     speaker = PyAudioSpeakerService()
+    """
     while True:
         # listen for input
 
--- client/src/software_services/wakeword_service.py
+++ client/src/software_services/wakeword_service.py
@@ -1,4 +1,44 @@
 """
-This Module is in charge of wake word detectiong
+This Module is in charge of wake word detection
 in audio (PCM Bytes).
 """
+
+from abc import ABC, abstractmethod
+import numpy as np
+from openwakeword.model import Model
+from client.src.config import (
+    WAKEWORD,
+    WAKEWORD_THRESHOLD,
+    PCM_BYTE_CHUNK_SIZE
+)
+
+
+class WakeWordDetectionService(ABC):
+    """
+    This class is an interface for wakeword detection service.
+    """
+
+    @abstractmethod
+    def detect_wakeword(self, pcm_bytes):
+        """This method detects a wake word in raw pcm bytes"""
+        pass
+
+
+class OpenWakeWordService(WakeWordDetectionService):
+    """
+    This class is an implementation for wakeword service using
+    openwakeword
+    """
+
+    def __init__(self, model: Model):
+        self.model = model
+
+    def detect_wakeword(self, pcm_bytes):
+        """This method detects a wake word in raw pcm bytes"""
+        if not isinstance(pcm_bytes, bytes):
+            raise ValueError(f"Expected bytes, got {type(pcm_bytes).__name__}")
+        if len(pcm_bytes) != PCM_BYTE_CHUNK_SIZE * 2:
+            raise ValueError(f"Expected {PCM_BYTE_CHUNK_SIZE * 2} bytes, got {len(pcm_bytes)}")
+        audio_array = np.frombuffer(pcm_bytes, dtype=np.int16)
+        prediction = self.model.predict(audio_array)
+        return prediction[WAKEWORD] >= WAKEWORD_THRESHOLD
